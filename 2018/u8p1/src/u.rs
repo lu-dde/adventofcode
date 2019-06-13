f11b40ff0203b0cfaaf6d5cf58099cede42e0863
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-06-13 19:59:10 +0200

    too slow, cant answer under 1s

--- 2018/u8p1/src/u.rs
+++ 2018/u8p1/src/u.rs
@@ -1,57 +1,80 @@
-use std::collections::*;
+#![feature(integer_atomics)]
+    
+use std::sync::atomic::*;
 
-type Dep = (char, char);
+static total_cost: AtomicI32 = AtomicI32::new(0);
 
-fn parse_deps(line: &str) -> Dep {
-    match scan_fmt!(
-        line,
-        "Step {} must be finished before step {} can begin.",
-        char,
-        char
-    ) {
-        Ok(t) => t,
-        _ => panic!("No such line"),
+#[derive(Debug)]
+struct Node {
+    meta: Vec<i32>,
+    nr: i32,
+    children: Box<Vec<Node>>,
+    to_parse: Box<Vec<i32>>,
+}
+
+impl Node {
+
+    pub fn new(to_parse: &[i32]) -> Node {
+        let (meta,c,children) = Node::parse_split(to_parse);
+        Node {
+            meta: meta,
+            nr: c,
+            to_parse: Box::new(children),
+            children: Box::new(vec![]),
+        }
     }
+
+    pub fn parse_split(to_parse: &[i32]) -> (Vec<i32>, i32, Vec<i32>) {
+        let (node, rest) = to_parse.split_at(2);
+        let c = node[0];
+        let m = node[1];
+
+        let children;
+        let meta;
+        if c == 0 {
+            let (l, r) = rest.split_at(m as usize);
+            children = r;
+            meta = l;
+        } else {
+            let (l, r) = rest.split_at(rest.len() - (m as usize));
+            children = l;
+            meta = r;
+        }
+        (meta.to_vec(),c,children.to_vec())
+    }
+
+    pub fn expand(&mut self) {
+        let t = &mut self.to_parse;
+        for _ in 0..self.nr {
+            let (m,n,c) = Node::parse_split(&t);
+            t.drain(..);
+            for a in (&c).iter() {
+                t.push(*a);
+            }
+            for &cost in (&m).iter() {
+                total_cost.fetch_add(cost,Ordering::Relaxed);
+            }
+            //println!("{} {}", n, total_cost.load(Ordering::Relaxed));
+            let mut n = Node { meta: m, nr: n, children: Box::new(vec![]), to_parse: Box::new(c) };
+            n.expand();
+            self.children.push( n );
+        }
+        drop(self.to_parse);
+    }
+
 }
 
 pub fn solve(text: String) -> String {
-    let deps: Vec<Dep> = text.lines().map(|c| parse_deps(c)).collect();
+    let numbers: Vec<_> = text
+        .split_whitespace()
+        .map(|s| s.parse::<i32>() )
+        .filter_map(Result::ok)
+        .collect();
 
-    let mut remaining: HashSet<char> = HashSet::new();
-    let mut disabled: HashMap<char, HashSet<char>> = HashMap::new();
+    // gör en vänster hänt consumer ät bara framåt i O(n)
+    // skippa träd. det är inget träd!
+    let mut n = Node::new(numbers.as_slice());
+    n.expand();
 
-    for d in &deps {
-        remaining.insert(d.0);
-        remaining.insert(d.1);
-        disabled.entry(d.1).or_default().insert(d.0);
-    }
-
-    let mut ds: Vec<char> = vec![];
-    loop {
-        let enabled: char;
-        match remaining
-            .iter()
-            .filter(|k| !disabled.contains_key(&k))
-            .min()
-        {
-            Some(c) => enabled = *c,
-            None => break,
-        }
-        print!("{}", enabled);
-        ds.push(enabled);
-        remaining.remove(&enabled);
-        deps.iter()
-            .filter(|(k, _)| *k == enabled)
-            .for_each(|(k, v)| {
-                let set = disabled.get_mut(v).unwrap();
-                set.remove(k);
-                if set.is_empty() {
-                    disabled.remove(v);
-                    //println!("enabled {} by {}", v, k);
-                }
-            });
-    }
-
-    println!();
-    format!("CABDFE {:?}", 0)
+    format!("TREE SUM {:?}", 0)
 }
