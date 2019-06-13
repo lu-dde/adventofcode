9758cd5e3657b7d698dcd62b3756703af363bc8a
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-06-13 19:58:41 +0200

    no need for hashset

--- 2018/u3p2/src/u.rs
+++ 2018/u3p2/src/u.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 type Square = (i32, (i32, i32), (i32, i32));
 
 fn pr(line: &str) -> Square {
@@ -17,23 +15,24 @@ fn overlapping_squares(p: Square, q: Square) -> bool {
     let (sq_id1, (a_l, a_t), (a_r, a_b)) = p;
     let (sq_id2, (b_l, b_t), (b_r, b_b)) = q;
     sq_id1 != sq_id2 && !(a_r < b_l || b_r < a_l || a_b < b_t || b_b < a_t)
+    // sq_id1 != sq_id2 && a_r >= b_l && b_r >= a_l && a_b >= b_t && b_b >= a_t
 }
 
 pub fn solve(text: String) -> String {
     let c1 = text.lines();
 
-    let mut passed = HashSet::new();
+    let mut passed = vec![];
     let m = c1.map(pr);
     let sq: Vec<Square> = m.collect();
     for head in (&sq).iter() {
         let id = &head.0;
-        match sq.iter().find(|other| overlapping_squares(*head, **other) ) {
+        match sq.iter().find(|&other| overlapping_squares(*head, *other)) {
             None => {
-                passed.insert(id);
+                passed.push(id);
             }
             _ => {}
         }
     }
-    
+
     format!("{:?}", passed)
 }
