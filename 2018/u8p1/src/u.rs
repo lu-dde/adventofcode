5d4d7d549a8a0b28287ab36b65bc3588f91e670e
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-06-13 19:59:11 +0200

    u8p1 answer is 37262 in 1ms

--- 2018/u8p1/src/u.rs
+++ 2018/u8p1/src/u.rs
@@ -1,80 +1,84 @@
-#![feature(integer_atomics)]
-    
-use std::sync::atomic::*;
-
-static total_cost: AtomicI32 = AtomicI32::new(0);
-
 #[derive(Debug)]
-struct Node {
-    meta: Vec<i32>,
-    nr: i32,
-    children: Box<Vec<Node>>,
-    to_parse: Box<Vec<i32>>,
-}
-
-impl Node {
-
-    pub fn new(to_parse: &[i32]) -> Node {
-        let (meta,c,children) = Node::parse_split(to_parse);
-        Node {
-            meta: meta,
-            nr: c,
-            to_parse: Box::new(children),
-            children: Box::new(vec![]),
-        }
-    }
-
-    pub fn parse_split(to_parse: &[i32]) -> (Vec<i32>, i32, Vec<i32>) {
-        let (node, rest) = to_parse.split_at(2);
-        let c = node[0];
-        let m = node[1];
-
-        let children;
-        let meta;
-        if c == 0 {
-            let (l, r) = rest.split_at(m as usize);
-            children = r;
-            meta = l;
-        } else {
-            let (l, r) = rest.split_at(rest.len() - (m as usize));
-            children = l;
-            meta = r;
-        }
-        (meta.to_vec(),c,children.to_vec())
-    }
-
-    pub fn expand(&mut self) {
-        let t = &mut self.to_parse;
-        for _ in 0..self.nr {
-            let (m,n,c) = Node::parse_split(&t);
-            t.drain(..);
-            for a in (&c).iter() {
-                t.push(*a);
-            }
-            for &cost in (&m).iter() {
-                total_cost.fetch_add(cost,Ordering::Relaxed);
-            }
-            //println!("{} {}", n, total_cost.load(Ordering::Relaxed));
-            let mut n = Node { meta: m, nr: n, children: Box::new(vec![]), to_parse: Box::new(c) };
-            n.expand();
-            self.children.push( n );
-        }
-        drop(self.to_parse);
-    }
-
+enum Action {
+    ReadHead,
+    Children(i32),
+    Eat(i32),
+    EndNode,
 }
 
 pub fn solve(text: String) -> String {
-    let numbers: Vec<_> = text
-        .split_whitespace()
-        .map(|s| s.parse::<i32>() )
-        .filter_map(Result::ok)
-        .collect();
+    let mut children: Vec<_> = vec![];
+    let mut meta: Vec<_> = vec![];
+    let mut sum: Vec<i32> = vec![];
+    let mut action: Action = Action::ReadHead;
 
-    // gör en vänster hänt consumer ät bara framåt i O(n)
-    // skippa träd. det är inget träd!
-    let mut n = Node::new(numbers.as_slice());
-    n.expand();
+    let mut t: std::iter::Peekable<_> = text
+        .split_whitespace()
+        .map(str::parse::<i32>)
+        .filter_map(Result::ok)
+        .peekable();
+
+    'outer: loop {
+        /*
+        println!();
+        println!();
+        println!("children  {:?}", children);
+        println!("meta      {:?}", meta);
+        println!("sum       {:?}", sum);
+        println!("action    {:?}", action);
+        */
+        match action {
+            Action::ReadHead => {
+                if t.peek().is_some() {
+                    let nr_of_children = t.next().unwrap();
+                    children.push(nr_of_children);
+                    let nr_of_meta = t.next().unwrap();
+                    meta.push(nr_of_meta);
+
+                    action = Action::Children(nr_of_children);
+                } else {
+                    panic!("ok no next");
+                }
+            }
+            Action::Children(0) => {
+                let meta = meta.pop().unwrap();
+                action = Action::Eat(meta);
+            }
+            Action::Children(_) => {
+                action = Action::ReadHead;
+            }
+            Action::Eat(0) => {
+                action = Action::EndNode;
+            }
+            Action::Eat(num) => {
+                let n = t.next().unwrap();
+                sum.push(n);
+                action = Action::Eat(num - 1);
+            }
+            Action::EndNode => {
+                if let Some(parent) = children.pop() {
+                    if parent > 0 {
+                        children.push(parent - 1);
+                        action = Action::Children(parent - 1);
+                    } else {
+                        action = Action::EndNode;
+                    }
+                } else {
+                    break 'outer;
+                }
+            }
+        }
+    }
+
+    sum.sort();
+
+    println!();
+    println!();
+    println!("children  {:?}", children);
+    println!("meta      {:?}", meta);
+    //println!("sum       {:?}", sum);
+    println!("sum       {:?}", sum.iter().fold(0, |x,y| x + y));
+    println!("action    {:?}", action);
 
     format!("TREE SUM {:?}", 0)
 }
