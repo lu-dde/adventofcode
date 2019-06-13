e72cd56df00c27b6095b4a5723578bd442083b5d
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-06-13 19:58:40 +0200

    fastest! 0.2s time cargo run --release

--- 2018/u3p2/src/u.rs
+++ 2018/u3p2/src/u.rs
@@ -1,42 +1,39 @@
-use std::collections::HashMap;
 use std::collections::HashSet;
 
-fn pr(line: &str) -> Vec<(i32, i32)> {
+type Square = (i32, (i32, i32), (i32, i32));
+
+fn pr(line: &str) -> Square {
     // #1281 @ 752,17: 29x18
     // #1282 @ 570,125: 19x10
-    let mut a: Vec<_> = vec![];
-    if let Ok((sq_id, x, y, dx, dy)) =
-        scan_fmt!(line, "#{d} @ {d},{d}: {d}x{d}", i32, i32, i32, i32, i32)
-    {
-        for i in x..(x + dx) {
-            for j in y..(y + dy) {
-                a.push((sq_id, (i * 1000) + j));
-            }
-        }
+    let mut t: Square = (0, (0, 0), (0, 0));
+    match scan_fmt!(line, "#{d} @ {d},{d}: {d}x{d}", i32, i32, i32, i32, i32) {
+        Ok((sq_id, x, y, dx, dy)) => t = (sq_id, (x, y), (x + dx - 1, y + dy - 1)),
+        _ => {}
     }
-    a
+    t
+}
+
+fn overlapping_squares(p: Square, q: Square) -> bool {
+    let (sq_id1, (a_l, a_t), (a_r, a_b)) = p;
+    let (sq_id2, (b_l, b_t), (b_r, b_b)) = q;
+    sq_id1 != sq_id2 && !(a_r < b_l || b_r < a_l || a_b < b_t || b_b < a_t)
 }
 
 pub fn solve(text: String) -> String {
     let c1 = text.lines();
 
-    let start = (HashSet::new(), HashMap::new());
-    let (invalid, _) = c1.flat_map(pr).fold(
-        start,
-        |(mut invalid, mut coords), (square_id, coordinate)| {
-            if let Some(old) = coords.insert(coordinate, square_id) {
-                invalid.insert(square_id);
-                invalid.insert(old);
+    let mut passed = HashSet::new();
+    let m = c1.map(pr);
+    let sq: Vec<Square> = m.collect();
+    for head in (&sq).iter() {
+        let id = &head.0;
+        match sq.iter().find(|other| overlapping_squares(*head, **other) ) {
+            None => {
+                passed.insert(id);
             }
-            (invalid, coords)
-        },
-    );
-
-    let mut t = 860016;
-
-    for x in invalid {
-        t -= x;
+            _ => {}
+        }
     }
-
-    format!("{}", t)
+    
+    format!("{:?}", passed)
 }
