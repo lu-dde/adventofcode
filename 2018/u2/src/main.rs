f61d27b5f820b8f0c68038aa6ddd3c6f1e122689
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-06-13 19:58:15 +0200

    remove unneeded boilerplate

--- 2018/u2/src/main.rs
+++ 2018/u2/src/main.rs
@@ -1,47 +1,34 @@
-use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
 mod u;
 
 fn main() {
-    let mut res = None;
-    let args: Vec<String> = env::args().collect();
-    match args.len() {
-        1 => {
-            println!("atleast 1 argument");
-        }
-        2 => {
-            let file_name = &args[1];
-            println!("reading file:'{}'", file_name);
-            let file = File::open(file_name);
+    let file = File::open("input");
+    let res = match file {
+        Ok(mut f) => {
             let mut buf = String::new();
-            match file {
-                Ok(mut file) => {
-                    match file.read_to_string(&mut buf) {
-                        Ok(_) => {
-                            res = Some(u::solve(buf));
-                        }
-                        Err(why) => {
-                            println!("ERROR: {:#?}", why);
-                        }
-                    }
+            match f.read_to_string(&mut buf) {
+                Ok(_) => {
+                    Some(u::solve(buf))
                 }
                 Err(why) => {
                     println!("ERROR: {:#?}", why);
+                    None
                 }
             }
         }
-        _ => {
-            println!("unmatching arguments");
+        Err(why) => {
+            println!("ERROR: {:#?}", why);
+            None
         }
-    }
+    };
 
     match res {
         Some(answer) => {
             println!("anwser is: {:#?}", answer);
         }
-        _ => {
+        None => {
             println!("unresolved");
         }
     }
