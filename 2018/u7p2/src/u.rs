edf265c00276a348ec68b637fd84f78b749eebd5
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-06-13 19:59:07 +0200

    u7p2 anwser is: 1234 in 3ms

--- 2018/u7p2/src/u.rs
+++ 2018/u7p2/src/u.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::collections::*;
 
 type Dep = (char, char);
@@ -14,6 +15,10 @@ fn parse_deps(line: &str) -> Dep {
     }
 }
 
+fn get_cost(c: char) -> (char,i32) {
+    (c, (c as i32) - 4)
+}
+
 pub fn solve(text: String) -> String {
     let deps: Vec<Dep> = text.lines().map(|c| parse_deps(c)).collect();
 
@@ -26,35 +31,61 @@ pub fn solve(text: String) -> String {
         disabled.entry(d.1).or_default().insert(d.0);
     }
 
+    let mut total_time = 0;
+    let mut workers: Vec<(char, i32)> = vec![];
     let max_workers = 5;
-    let mut workers: Vec<(char,i32)> = vec![];
 
-    let mut ds: Vec<char> = vec![];
-    loop {
-        let enabled: char;
-        match remaining
-            .iter()
-            .filter(|k| !disabled.contains_key(&k))
-            .min()
-        {
-            Some(c) => enabled = *c,
-            None => break,
-        }
-        print!("{}", enabled);
-        ds.push(enabled);
-        remaining.remove(&enabled);
-        deps.iter()
+    let first = remaining
+        .iter()
+        .filter(|k| !disabled.contains_key(&k))
+        .min()
+        .unwrap();
+    workers.push(get_cost(*first));
+
+    let mut answer: String = "".to_string();
+
+    let mut ready: Vec<char> = vec![];
+    while !workers.is_empty() {
+        workers.sort_by_key(|(c, i)| *i * 100 + (*c as i32));
+        let (enabled, time_passed) = workers.remove(0);
+        workers = workers
+            .into_iter()
+            .map(|(c, t)| (c, t - time_passed))
+            .collect();
+        total_time += time_passed;
+
+        println!(
+            "popped {} at time {} after {}s",
+            enabled, total_time, time_passed
+        );
+
+        answer.push(enabled);
+
+        let mut new_ready: Vec<char> = deps.iter()
             .filter(|(k, _)| *k == enabled)
-            .for_each(|(k, v)| {
+            .filter_map(|(k, v)| {
                 let set = disabled.get_mut(v).unwrap();
                 set.remove(k);
                 if set.is_empty() {
+                    println!("     enabled: {}", v);
                     disabled.remove(v);
-                    //println!("enabled {} by {}", v, k);
+                    Some(v)
+                } else {
+                    None
                 }
-            });
+            }).cloned().collect();
+        ready.append(&mut new_ready);
+
+        ready.sort();
+
+        println!("     READY: {:?}", ready);
+        let min = cmp::min(ready.len(), max_workers - workers.len());
+        for _ in 0..min {
+            let new_char = ready.remove(0);
+            workers.push(get_cost(new_char));
+        }
+        println!("     took: {} state: {:?}", min, workers);
     }
 
-    println!();
-    format!("CABDFE {:?}", 0)
+    format!("total_time {}", total_time)
 }
