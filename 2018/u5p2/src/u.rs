b179cfe776c72c554429bd7d6694d5eba28ca46b
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-06-13 19:58:55 +0200

    u5p2 answer: 6310 in 16ms

--- 2018/u5p2/src/u.rs
+++ 2018/u5p2/src/u.rs
@@ -4,41 +4,44 @@ fn eq_opposite_case(a: char, b: char) -> bool {
     a != b && a.eq_ignore_ascii_case(&b)
 }
 
+fn react(skip_char: char, text: &Vec<char>) -> usize {
+    let length = text
+        .iter()
+        .filter(|c| !skip_char.eq_ignore_ascii_case(&c))
+        .fold(vec![], |mut f, c| {
+            let option_fpop = f.pop();
+            match option_fpop {
+                Some(c_c) => {
+                    if !eq_opposite_case(c_c, *c) {
+                        f.push(c_c);
+                        f.push(*c);
+                    }
+                }
+                None => {
+                    f.push(*c);
+                }
+            }
+            f
+        })
+        .len();
+    length
+}
+
 pub fn solve(text: String) -> String {
-    let c1 = text.chars();
+    let c1: Vec<char> = text
+        .chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .collect();
 
     let mut taken = HashSet::new();
 
-    let min = c1
-        .into_iter()
-        .filter(|c| c.is_ascii_alphabetic())
+    let min = (&c1)
+        .iter()
         .map(|c| c.to_ascii_lowercase())
         .filter(|c| taken.insert(c.clone()))
-        .map(|skip_c| {
-            let length = text
-                .chars()
-                .filter(|c| !skip_c.eq_ignore_ascii_case(&c))
-                .fold(vec![], |mut f, c| {
-                    let option_fpop = f.pop();
-                    match option_fpop {
-                        Some(cC) => {
-                            if !eq_opposite_case(cC, c) {
-                                f.push(cC);
-                                f.push(c);
-                            }
-                        }
-                        None => {
-                            f.push(c);
-                        }
-                    }
-                    f
-                })
-                .len();
-            println!("{} {}", skip_c, length);
-            length
-        })
+        .map(|skip_char: char| react(skip_char, &c1))
         .min()
         .unwrap();
 
-    format!("{:?}", min)
+    format!("{}", min)
 }
