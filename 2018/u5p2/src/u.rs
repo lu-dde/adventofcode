cb4a9361a64b26b5c78a45abf9d2c239a28a9ed3
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-06-13 19:58:53 +0200

    u5p2 answer: 6311 was too high

--- 2018/u5p2/src/u.rs
+++ 2018/u5p2/src/u.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn eq_opposite_case(a: char, b: char) -> bool {
     a != b && a.eq_ignore_ascii_case(&b)
 }
@@ -5,27 +7,38 @@ fn eq_opposite_case(a: char, b: char) -> bool {
 pub fn solve(text: String) -> String {
     let c1 = text.chars();
 
-    let f: String = c1
-        .filter(|c| c.is_ascii_alphabetic() )
-        .rev()
-        .fold(vec![], |mut f, c| {
-            let option_fpop = f.pop();
-            match option_fpop {
-                Some(cC) => {
-                    if !eq_opposite_case(cC, c) {
-                        f.push(cC);
-                        f.push(c);
-                    }
-                }
-                None => {
-                    f.push(c);
-                }
-            }
-            f
-        })
-        .into_iter()
-        .rev()
-        .collect();
+    let mut taken = HashSet::new();
 
-    format!("{}", f.len())
+    let min = c1
+        .into_iter()
+        .filter(|c| c.is_ascii_alphabetic())
+        .map(|c| c.to_ascii_lowercase())
+        .filter(|c| taken.insert(c.clone()))
+        .map(|skip_c| {
+            let length = text
+                .chars()
+                .filter(|c| !skip_c.eq_ignore_ascii_case(&c))
+                .fold(vec![], |mut f, c| {
+                    let option_fpop = f.pop();
+                    match option_fpop {
+                        Some(cC) => {
+                            if !eq_opposite_case(cC, c) {
+                                f.push(cC);
+                                f.push(c);
+                            }
+                        }
+                        None => {
+                            f.push(c);
+                        }
+                    }
+                    f
+                })
+                .len();
+            println!("{} {}", skip_c, length);
+            length
+        })
+        .min()
+        .unwrap();
+
+    format!("{:?}", min)
 }
