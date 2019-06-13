4a1f8ac04e1ac1de6e892523e90c7599eec4daa6
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-06-13 19:58:58 +0200

    u5p1 speedup to 0ms

--- 2018/u5p1/src/u.rs
+++ 2018/u5p1/src/u.rs
@@ -5,9 +5,8 @@ fn eq_opposite_case(a: char, b: char) -> bool {
 pub fn solve(text: String) -> String {
     let c1 = text.chars();
 
-    let f: String = c1
+    let f = c1
         .filter(|c| c.is_ascii_alphabetic() )
-        .rev()
         .fold(vec![], |mut f, c| {
             let option_fpop = f.pop();
             match option_fpop {
@@ -23,9 +22,7 @@ pub fn solve(text: String) -> String {
             }
             f
         })
-        .into_iter()
-        .rev()
-        .collect();
+        .len();
 
-    format!("{}", f.len())
+    format!("{}", f)
 }
