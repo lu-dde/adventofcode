2399e949ba8e194cfcd7e3939648ed40c6dde56f
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-06-13 19:58:21 +0200

    wrong thoughts

--- 2018/u3/src/u.rs
+++ 2018/u3/src/u.rs
@@ -1,25 +1,31 @@
-use std::collections::HashSet;
 
-pub fn solve(lines: String) -> String {
+pub fn solve(text: String) -> String {
+    let c1 = text.lines();
+    let c2 = text.lines().skip(1);
 
-    fn to_u32(s: &str) -> i32 { s.parse().unwrap() }
-
-    let mut reached = HashSet::new();
-
-    let mut i = lines.lines().map(to_u32).cycle();
-
-    let mut acc = 0;
-    loop {
-        match i.next() {
-            Some(n) => {
-                acc += n;
-                if ! reached.insert(acc) {
-                    break
-                }
-            }
-            _ => { break }
-        }
+    fn find_distance<'a>(pair: (&'a str, &'a str)) -> (&'a str,&'a str,usize) {
+       let (p,q) = pair; 
+       let r = p.chars().zip(q.chars()).fold(0,|acc, (p,q)|acc + (p == q) as usize );
+       println!("{:?}",(p,q,r));
+       (p,q,r)
     }
 
-    acc.to_string()
+    let (p,q,i): (&str,&str,usize) = c1.zip(c2)
+        .map(find_distance)
+        .max_by(|a,b| a.2.cmp(&b.2))
+        .unwrap();
+
+    let s: Vec<char> = p.chars().zip(q.chars())
+        .filter(|(a,b)| a == b)
+        .map(|(a,_)|a)
+        .collect();
+
+    let mut key = String::new();
+    for c in s {
+        key.push(c);
+    }
+
+    println!("{:?} {}", key, key.len());
+
+    format!("{} = {} {} {}", p, q, i, p.len())
 }
