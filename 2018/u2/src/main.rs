11edcba0cf68771dff7a2e06dd5198187d1c74c2
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-06-13 19:58:19 +0200

    minify main.rs

--- 2018/u2/src/main.rs
+++ 2018/u2/src/main.rs
@@ -4,32 +4,20 @@ use std::io::prelude::*;
 mod u;
 
 fn main() {
-    let file = File::open("input");
-    let res = match file {
+    match File::open("input") {
         Ok(mut f) => {
             let mut buf = String::new();
             match f.read_to_string(&mut buf) {
                 Ok(_) => {
-                    Some(u::solve(buf))
+                    println!("anwser is: {:#?}", u::solve(buf));
                 }
                 Err(why) => {
                     println!("ERROR: {:#?}", why);
-                    None
                 }
             }
         }
         Err(why) => {
             println!("ERROR: {:#?}", why);
-            None
-        }
-    };
-
-    match res {
-        Some(answer) => {
-            println!("anwser is: {:#?}", answer);
-        }
-        None => {
-            println!("unresolved");
         }
     }
 }
