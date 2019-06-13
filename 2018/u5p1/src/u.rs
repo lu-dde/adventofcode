bd1f44b540ccde54f8cfde71415b72a6ac7c9a93
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-06-13 19:58:51 +0200

    u5p1 answer: 9060 in 0ms?

--- 2018/u5p1/src/u.rs
+++ 2018/u5p1/src/u.rs
@@ -1,6 +1,31 @@
+fn eq_opposite_case(a: char, b: char) -> bool {
+    a != b && a.eq_ignore_ascii_case(&b)
+}
 
 pub fn solve(text: String) -> String {
     let c1 = text.chars();
 
-    format!("{}", 0)
+    let f: String = c1
+        .filter(|c| c.is_ascii_alphabetic() )
+        .rev()
+        .fold(vec![], |mut f, c| {
+            let option_fpop = f.pop();
+            match option_fpop {
+                Some(cC) => {
+                    if !eq_opposite_case(cC, c) {
+                        f.push(cC);
+                        f.push(c);
+                    }
+                }
+                None => {
+                    f.push(c);
+                }
+            }
+            f
+        })
+        .into_iter()
+        .rev()
+        .collect();
+
+    format!("{}", f.len())
 }
