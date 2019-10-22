af8c300dfd82078fec3a6094fced431bfc6ccade
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-10-22 16:27:23 +0200

    GO: solution '5704' in 1.477162ms
    2019/10/22 16:27:14 time taken: 1.477162ms
    2019/10/22 16:27:14 Solution: 5704

--- go/2018/u2p1/solve.go
+++ go/2018/u2p1/solve.go
@@ -2,22 +2,50 @@ package main
 
 import (
 	"fmt"
-	"log"
 )
+
+func counts(l string) (int, int) {
+	m := make(map[rune]int32, 26)
+	for _, c := range l {
+		_, prev := m[c]
+		if prev {
+			m[c]++
+		} else {
+			m[c] = 1
+		}
+	}
+
+	c2 := 0
+	c3 := 0
+
+	for _, value := range m {
+		if value == 2 {
+			c2 = 1
+		}
+		if value == 3 {
+			c3 = 1
+		}
+	}
+
+	return c2, c3
+}
 
 func solve(p chan string, s chan string) {
 
-	sum := 0
+	c2 := 0
+	c3 := 0
 
 	for {
 		line, ok := <-p
 		if ok {
-			log.Print(line)
+			nc2, nc3 := counts(line)
+			c2 += nc2
+			c3 += nc3
 		} else {
 			break
 		}
 	}
 
-	s <- fmt.Sprintf("Solution: %d", sum)
+	s <- fmt.Sprintf("Solution: %d", c2*c3)
 	return
 }
