13c9d533ece60373bd02b3cec3d4a6c00761ddd0
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-12-05 00:04:45 +0100

    init u22 from u21

--- go/2019/get.go
+++ go/2019/get.go
@@ -7,6 +7,7 @@ func getSolver(name string) solver {
 		"11": U11,
 		"12": U12,
 		"21": U21,
+		"22": U22,
 	}
 
 	return functions[name]
@@ -17,6 +18,7 @@ func getTestfile(name string) string {
 		"11": "input/u11.txt",
 		"12": "input/u11.txt",
 		"21": "input/u21.txt",
+		"22": "input/u21.txt",
 	}
 
 	return textfiles[name]
---
+++ go/2019/u22.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//U22 is main proxy for solve, takes a string channel
+func U22(p chan string, s chan string) {
+
+	// we only expect one line.
+	line, _ := <-p
+
+	code := strings.Split(line, ",")
+	var ops = []int{}
+
+	for _, c := range code {
+		i, _ := strconv.Atoi(c)
+		ops = append(ops, i)
+	}
+
+	ops[1] = 12
+	ops[2] = 2
+
+	step := 4
+	var pos = 0
+
+oploop:
+	for {
+		opcode := ops[pos]
+
+		switch opcode {
+		case 99:
+			fmt.Println("99 :: break loop")
+			break oploop
+		case 1:
+			source1 := ops[pos+1]
+			source2 := ops[pos+2]
+			values1 := ops[source1]
+			values2 := ops[source2]
+			target := ops[pos+3]
+			ops[target] = values1 + values2
+		case 2:
+			source1 := ops[pos+1]
+			source2 := ops[pos+2]
+			values1 := ops[source1]
+			values2 := ops[source2]
+			target := ops[pos+3]
+			ops[target] = values1 * values2
+		}
+
+		pos += step
+	}
+
+	s <- fmt.Sprintf("Solution: %d", ops[0])
+}
