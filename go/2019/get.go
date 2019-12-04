9813edbe667ef3728ae9b4053f70e9836a2e76bb
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-12-04 23:58:56 +0100

    Day 2 Part 2 'Solution: 3409710' in 118.375µs

--- go/2019/get.go
+++ go/2019/get.go
@@ -6,6 +6,7 @@ func getSolver(name string) solver {
 	functions := map[string]solver{
 		"11": U11,
 		"12": U12,
+		"21": U21,
 	}
 
 	return functions[name]
@@ -15,6 +16,7 @@ func getTestfile(name string) string {
 	textfiles := map[string]string{
 		"11": "input/u11.txt",
 		"12": "input/u11.txt",
+		"21": "input/u21.txt",
 	}
 
 	return textfiles[name]
