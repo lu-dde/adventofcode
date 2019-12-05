cb80b5a81277bd3ccbb8011470b8ed6cdf5384ae
lu-dde <51797401+lu-dde@users.noreply.github.com>
2019-12-05 11:57:51 +0100

    Day 2 Part 2 'Solution: 7912' in 7.175295ms

--- go/2019/u22.go
+++ go/2019/u22.go
@@ -13,45 +13,81 @@ func U22(p chan string, s chan string) {
 	line, _ := <-p
 
 	code := strings.Split(line, ",")
-	var ops = []int{}
+	var cps = []int{}
 
 	for _, c := range code {
 		i, _ := strconv.Atoi(c)
-		ops = append(ops, i)
+		cps = append(cps, i)
 	}
 
-	ops[1] = 12
-	ops[2] = 2
+	opcodeChan := make(chan []int, 100*100)
+	solutions := make(chan sol, 100*100)
 
-	step := 4
-	var pos = 0
+	go opscode(opcodeChan, solutions)
 
-oploop:
-	for {
-		opcode := ops[pos]
+	for noun := 0; noun <= 99; noun++ {
+		for verb := 0; verb <= 99; verb++ {
+			var ops = make([]int, len(cps))
+			copy(ops, cps)
 
-		switch opcode {
-		case 99:
-			fmt.Println("99 :: break loop")
-			break oploop
-		case 1:
-			source1 := ops[pos+1]
-			source2 := ops[pos+2]
-			values1 := ops[source1]
-			values2 := ops[source2]
-			target := ops[pos+3]
-			ops[target] = values1 + values2
-		case 2:
-			source1 := ops[pos+1]
-			source2 := ops[pos+2]
-			values1 := ops[source1]
-			values2 := ops[source2]
-			target := ops[pos+3]
-			ops[target] = values1 * values2
+			ops[1] = noun
+			ops[2] = verb
+
+			opcodeChan <- ops
+		}
+	}
+
+	for solution := range solutions {
+		if solution.result == 19690720 {
+			s <- fmt.Sprintf("Solution: %d, %v", 100*solution.noun+solution.verb, solution)
+			break
+		}
+	}
+
+}
+
+type sol struct {
+	noun   int
+	verb   int
+	result int
+}
+
+func opscode(opsChan chan []int, solution chan sol) {
+
+	for ops := range opsChan {
+		n := ops[1]
+		v := ops[2]
+
+		step := 4
+		var pos = 0
+
+	oploop:
+		for {
+			opcode := ops[pos]
+
+			switch opcode {
+			case 99:
+				break oploop
+			case 1:
+				source1 := ops[pos+1]
+				source2 := ops[pos+2]
+				values1 := ops[source1]
+				values2 := ops[source2]
+				target := ops[pos+3]
+				ops[target] = values1 + values2
+			case 2:
+				source1 := ops[pos+1]
+				source2 := ops[pos+2]
+				values1 := ops[source1]
+				values2 := ops[source2]
+				target := ops[pos+3]
+				ops[target] = values1 * values2
+			}
+
+			pos += step
 		}
 
-		pos += step
+		solution <- sol{noun: n, verb: v, result: ops[0]}
 	}
 
-	s <- fmt.Sprintf("Solution: %d", ops[0])
 }
