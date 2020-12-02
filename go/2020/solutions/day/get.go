5731f46e88c7451d9baee21de8eec736bb3207ce
lu-dde <51797401+lu-dde@users.noreply.github.com>
2020-12-02 20:35:06 +0100

    add path to day example

--- go/2020/solutions/day/get.go
+++ go/2020/solutions/day/get.go
@@ -11,20 +11,22 @@ const day string = "DAY"
 //Problems is a list of problems for DAY
 func Problems() []solver.Problem {
 
+	path := "solutions/u" + day
+
 	solvers := []solver.Problem{
 		{
 			Key:       day + "p1",
 			Day:       day,
 			Part:      "1",
 			Solve:     p1.Solve1,
-			InputFile: "solutions/u1/input.txt",
+			InputFile: path + "/input.txt",
 		},
 		{
 			Key:       day + "p2",
 			Day:       day,
 			Part:      "2",
 			Solve:     p2.Solve2,
-			InputFile: "solutions/u1/input.txt",
+			InputFile: path + "/input.txt",
 		},
 	}
 
