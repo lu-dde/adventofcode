9137397f2be8a07ee3a42a7384c2e937be33401a
lu-dde <51797401+lu-dde@users.noreply.github.com>
2020-12-09 18:46:22 +0100

    2020 Day 9 Part 2 'Solution: 2' in 125.482µs

--- go/2020/solutions/u9/p2/solve.go
+++ go/2020/solutions/u9/p2/solve.go
@@ -5,46 +5,52 @@ import (
 	"strconv"
 )
 
-//var weaknessPos int = 14
-
-var weaknessPos int = 659
-
 //Solve is main proxy for solve, takes a string channel
 func Solve(p chan string, s chan string) {
 	var t uint64 = 0
 
-	length := weaknessPos + 1
-	loop := make([]uint64, length)
-	for i := range loop {
+	var weakness uint64 = 1492208709
+
+	length := 660
+	var sum uint64 = 0
+	sums := make([]uint64, length)
+	for i := range sums {
 		v, _ := strconv.ParseUint(<-p, 10, 64)
-		loop[i] = v
+		sum += v
+		sums[i] = sum
+		if v > weakness {
+			break
+		}
 	}
 
-	//	fmt.Println(loop)
-	var weakness uint64 = loop[weaknessPos]
+	var I int = 0
+	var J int = 0
 
 outer:
-	for i := 0; i < length; i++ {
-		sum := loop[i]
-		min := sum
-		max := sum
-
-		for j := i + 1; j < length; j++ {
-			cur := loop[j]
-			sum += cur
-			if min > cur {
-				min = cur
-			}
-			if max < cur {
-				max = cur
-			}
-			if sum == weakness {
-				t = min + max
+	for i := length - 1; i >= 0; i-- {
+		for j := i - 1; j >= 0; j-- {
+			if sums[i]-sums[j] == weakness {
+				I, J = i, j
 				break outer
+			} else if sums[i]-sums[j] < weakness {
+				continue outer
 			}
 		}
 
 	}
+
+	var min uint64 = sums[I+1] - sums[I]
+	var max uint64 = min
+	for i := J; i < I; i++ {
+		current := sums[i+1] - sums[i]
+		if current < min {
+			min = current
+		}
+		if current > max {
+			max = current
+		}
+	}
+	t = min + max
 
 	s <- fmt.Sprintf("Solution: %d", t)
 }
