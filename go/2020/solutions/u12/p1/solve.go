6d5eae8e9eaee056bc844239f24acaed7be23582
lu-dde <51797401+lu-dde@users.noreply.github.com>
2020-12-13 02:01:03 +0100

    2020 Day 12 Part 1 'Solution: 1177' in 136.518µs

--- go/2020/solutions/u12/p1/solve.go
+++ go/2020/solutions/u12/p1/solve.go
@@ -2,6 +2,7 @@ package p1
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
@@ -9,20 +10,67 @@ import (
 func Solve(p chan string, s chan string) {
 	var t = 0
 
-	var want map[int]int = make(map[int]int, 200)
+	x := 0.0
+	y := 0.0
+
+	var dir byte = 'E'
 
 	for line := range p {
-		i, _ := strconv.Atoi(line)
+		d := line[0]
+		i, _ := strconv.ParseFloat(line[1:], 64)
 
-		if want[i] > 0 {
-			t = want[i] * i
-			fmt.Println(want[i], "*", i, "=", want[i]*i)
-		} else {
-			want[2020-i] = i
-			fmt.Println("add want", 2020-i, "from", i)
+		switch d {
+		case 'F':
+			d = dir
+		case 'L':
+			dir = right(dir, 360-i)
+			d = dir
+			i = 0
+		case 'R':
+			dir = right(dir, i)
+			d = dir
+			i = 0
+		}
 
+		x, y = move(d, i, x, y)
+
+	}
+
+	t = int(math.Abs(x) + math.Abs(y))
+
+	s <- fmt.Sprintf("Solution: %d", t)
+}
+
+func right(d byte, deg float64) byte {
+	t := int(deg / 90)
+
+	for i := 0; i < t; i++ {
+		switch d {
+		case 'N':
+			d = 'E'
+		case 'E':
+			d = 'S'
+		case 'S':
+			d = 'W'
+		case 'W':
+			d = 'N'
 		}
 	}
 
-	s <- fmt.Sprintf("Solution: %d", t)
+	return d
+}
+
+func move(d byte, i, x, y float64) (float64, float64) {
+	switch d {
+	case 'N':
+		x += i
+	case 'S':
+		x -= i
+	case 'W':
+		y -= i
+	case 'E':
+		y += i
+	}
+
+	return x, y
 }
--- go/2020/solutions/u12/p2/solve.go
+++ go/2020/solutions/u12/p2/solve.go
@@ -2,6 +2,7 @@ package p2
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
@@ -9,25 +10,67 @@ import (
 func Solve(p chan string, s chan string) {
 	var t = 0
 
-	for {
-		line, ok := <-p
-		if ok {
-			i, _ := strconv.Atoi(line)
-			t += getFuel(i)
-		} else {
-			break
+	x := 0.0
+	y := 0.0
+
+	var dir byte = 'E'
+
+	for line := range p {
+		d := line[0]
+		i, _ := strconv.ParseFloat(line[1:], 64)
+
+		switch d {
+		case 'F':
+			d = dir
+		case 'L':
+			dir = right(dir, 360-i)
+			d = dir
+			i = 0
+		case 'R':
+			dir = right(dir, i)
+			d = dir
+			i = 0
 		}
+
+		x, y = move(d, i, x, y)
+
 	}
+
+	t = int(math.Abs(x) + math.Abs(y))
 
 	s <- fmt.Sprintf("Solution: %d", t)
 }
 
-func getFuel(i int) int {
-	if i < 9 {
-		return 0
+func right(d byte, deg float64) byte {
+	t := int(deg / 90)
+
+	for i := 0; i < t; i++ {
+		switch d {
+		case 'N':
+			d = 'E'
+		case 'E':
+			d = 'S'
+		case 'S':
+			d = 'W'
+		case 'W':
+			d = 'N'
+		}
 	}
 
-	r := i/3 - 2
-	return r + getFuel(r)
+	return d
+}
 
+func move(d byte, i, x, y float64) (float64, float64) {
+	switch d {
+	case 'N':
+		x += i
+	case 'S':
+		x -= i
+	case 'W':
+		y -= i
+	case 'E':
+		y += i
+	}
+
+	return x, y
 }
