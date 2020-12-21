1fa596cec54d9f9b824f07be1bbbd90a7049c669
lu-dde <51797401+lu-dde@users.noreply.github.com>
2020-12-21 19:36:02 +0100

    2020 Day 15 Part 2 'Solution: 8984' in 5.628401814s

--- go/2020/solutions/u15/p2/solve.go
+++ go/2020/solutions/u15/p2/solve.go
@@ -2,16 +2,52 @@ package p2
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
+	"time"
 )
 
 //Solve is main proxy for solve, takes a string channel
 func Solve(p chan string, s chan string) {
+	start := time.Now()
+
 	var t = 0
 
-	nums := strings.Split(<-p, ",")
+	numsString := strings.Split(<-p, ",")
 
-	t = len(nums)
+	last := 0
+
+	spoken := map[int][]int{}
+	for turn, ns := range numsString {
+		n, _ := strconv.Atoi(ns)
+		spoken[n] = append(spoken[n], turn)
+		spoken[n] = append(spoken[n], turn)
+		last = n
+	}
+
+	for turn := len(numsString); turn < 30000000; turn++ {
+		l := spoken[last]
+		firstSpokenTurn := l[0]
+		prevSpokenturn := l[len(l)-2]
+		lastSpokenturn := l[len(l)-1]
+		//fmt.Println("turn", turn, "last", last, firstSpokenTurn, prevSpokenturn, lastSpokenturn)
+		if lastSpokenturn == firstSpokenTurn {
+			spoken[0] = append(spoken[0], turn)
+			last = 0
+		} else {
+			last = lastSpokenturn - prevSpokenturn
+			_, notFirst := spoken[last]
+			if !notFirst {
+				spoken[last] = append(spoken[last], turn)
+			}
+			spoken[last] = append(spoken[last], turn)
+		}
+	}
+	//fmt.Println(spoken)
+
+	t = last
 
 	s <- fmt.Sprintf("Solution: %d", t)
+
+	fmt.Println("end", time.Since(start))
 }
