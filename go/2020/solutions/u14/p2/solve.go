f9b71b518af1f72243111185bba13f564654025e
lu-dde <51797401+lu-dde@users.noreply.github.com>
2020-12-21 18:11:18 +0100

    2020 Day 14 Part 2 'Solution: 3435342392262' in 11.586495ms

--- go/2020/solutions/u14/p2/solve.go
+++ go/2020/solutions/u14/p2/solve.go
@@ -10,7 +10,7 @@ import (
 func Solve(p chan string, s chan string) {
 	var t uint64 = 0
 
-	store := make(map[int]uint64, 0)
+	store := make(map[uint64]uint64, 0)
 
 	var m mask
 
@@ -19,11 +19,14 @@ func Solve(p chan string, s chan string) {
 
 		if l[0] == "mask" {
 			m.update(l[2])
+			//fmt.Println("M ", l[2])
 		} else {
-			index := 0
+			var index uint64 = 0
 			fmt.Sscanf(l[0], "mem[%d]", &index)
 			s, _ := strconv.ParseUint(l[2], 10, 35)
-			store[index] = m.mask(s)
+			for _, a := range m.mask(index) {
+				store[a] = s
+			}
 		}
 	}
 
@@ -35,34 +38,78 @@ func Solve(p chan string, s chan string) {
 }
 
 type mask struct {
-	and uint64
-	or  uint64
+	nand uint64
+	gen  uint64
+	or   uint64
 }
 
-func (m *mask) mask(n uint64) uint64 {
-	fmt.Printf("IN %036b (dec %d)\n", n, n)
-	fmt.Printf("&  %036b\n", m.and)
-	fmt.Printf("|  %036b\n", m.or)
-	fmt.Printf("=  %036b\n", (n&m.and)|m.or)
-	fmt.Println()
-	return n&m.and | m.or
+func (m *mask) mask(n uint64) []uint64 {
+	/*
+		fmt.Printf("IN %036b (dec %d)\n", n, n)
+		fmt.Printf("G  %036b %d\n", m.gen, bits.OnesCount64(m.gen))
+		fmt.Printf("&  %036b\n", m.nand)
+		fmt.Printf("|  %036b\n", m.or)
+		fmt.Printf("B  %036b\n", n&m.nand|m.or)
+	*/
+	return m.perm(n)
+}
+func (m *mask) perm(n uint64) []uint64 {
+	base := n&m.nand | m.or
+	perms := m.gen
+	parts := []uint64{}
+	var power uint64 = 1
+	for i := 0; i < 36; i++ {
+		if perms%2 == 1 {
+			parts = append(parts, power)
+		}
+		perms >>= 1
+		power <<= 1
+	}
+
+	combinations := 1 << len(parts)
+
+	res := make([]uint64, combinations)
+
+	for i := 0; i < combinations; i++ {
+		b := i
+		index := 0
+		var current uint64 = 0
+
+		for {
+			if b == 0 {
+				break
+			}
+			if b&1 == 1 {
+				current += parts[index]
+			}
+			b >>= 1
+			index++
+		}
+		res[i] = base + current
+		//fmt.Printf("=  %036b\n", res[i])
+	}
+	return res
 }
 
 func (m *mask) update(l string) {
 
-	var and, or uint64 = 0, 0
+	var nand, gen, or uint64 = 0, 0, 0
 
 	for _, x := range l {
-		and <<= 1
+		gen <<= 1
 		or <<= 1
+		nand <<= 1
 		switch x {
 		case 'X':
-			and++
+			gen++
 		case '1':
 			or++
+		case '0':
+			nand++
 		}
 	}
 
-	m.and = and
+	m.gen = gen
+	m.nand = nand
 	m.or = or
 }
